48d1d6e537570820f10de415a565a05c00fabcdc
Oksana Harr <oksana.harris@gmail.com>
2017-06-19 18:28:44 -1000

    color filling and click and drag working

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,4 +1,6 @@
-let width
+/*jshint esversion: 6 */
+
+let width;
 let height=4;
 
 let canvasDiv=document.createElement("div");
@@ -7,20 +9,44 @@ canvasDiv.id='pp-canvas';
 var pixelPainter=document.getElementById("pixelPainter");
 pixelPainter.appendChild(canvasDiv);
 
+var clickHappenYet = false;
+
+var selectedColor = 'green';
 
 
-function grid(height, width){
- for(i=0; i<height; i++){
-
-    for(j=0; j<width; j++){
+function grid(height, width, classStr){
+ for(i=1; i<=height; i++){
+    for(j=1; j<=width; j++){
       var newCell=document.createElement('div');
-      newCell.className='canvasCells';
+      newCell.id = 'cell' + i + j;
+      newCell.className=classStr;
+      newCell.addEventListener('mousedown', fillColorOnClick);
+      newCell.addEventListener('mouseenter', fillColorOnHover);
+      newCell.addEventListener('mouseup', fillColorOnMouseUp);
       canvasDiv.appendChild(newCell);
-
     }
     var lineBreak=document.createElement('br');
     canvasDiv.appendChild(lineBreak);
  }
 
 }
-grid(2,3);
+grid(10,10, 'canvasCells');
+
+function fillColorOnHover (e){
+  if(clickHappenYet === true){
+    var cellId = e.target.id;
+    var cell = document.getElementById(cellId);
+    cell.style.backgroundColor = selectedColor;
+  }
+}
+
+function fillColorOnMouseUp (e){
+  clickHappenYet = false;
+}
+
+function fillColorOnClick (e){
+  clickHappenYet = true;
+  var cellId = e.target.id;
+  var cell = document.getElementById(cellId);
+  cell.style.backgroundColor = selectedColor;
+}
